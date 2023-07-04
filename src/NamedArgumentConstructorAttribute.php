aa45e59e0d50119d237177a897ee801efce3b8d9
Maxim Smakouz <m.smakouz@gmail.com>
2023-07-04 17:18:50 +0300

    NamedArgumentConstructor: load polyfill explicitly (#8)

--- src/NamedArgumentConstructorAttribute.php
+++ src/NamedArgumentConstructorAttribute.php
@@ -6,6 +6,18 @@ namespace Spiral\Attributes;
 
 use Doctrine\Common\Annotations\NamedArgumentConstructorAnnotation;
 
+//
+// In some cases, the polyfill may not load. For example, if this class is
+// loaded from the composer plugin (plugins do not load the files defined in
+// the "require.classmap" and "require.files" section of the "composer.json"
+// file).
+//
+// In this case, it should be loaded explicitly.
+//
+if (!\class_exists(NamedArgumentConstructorAnnotation::class, false)) {
+    require_once __DIR__ . '/polyfill.php';
+}
+
 /**
  * Marker interface for PHP7/PHP8 compatible support for named arguments
  * (and constructor property promotion).
